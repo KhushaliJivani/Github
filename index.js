34dd278c8d5ef3e9a1a9c8daf8c4a63704a1305b
Khushali <Khushalijivani31@gmail.com>
2022-08-28 16:11:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+
+const port=5000
+const app = express()
+
+app.get('/data',async(req,res)=>{
+    res.send('request...............................')
+    console.log(req);        
+})
+
+app.listen(port,()=>{
+    console.log("server is listen on port.",port)
+})
